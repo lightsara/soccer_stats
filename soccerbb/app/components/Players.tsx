d1b03858605590172cd68427873eac96ec713c4c
salight <light.sara@icloud.com>
2025-04-07 14:13:06 +0200

    added player object

---
+++ soccerbb/app/components/Players.tsx
@@ -0,0 +1,34 @@
+"use client"    //must be client side because of the use of useeffect and usestate
+import React, { useEffect, useState } from "react";
+
+interface ProlificGoalScorer {
+    playerId: number;
+    playername: string;
+    goalcount: number;
+}
+
+const Players: React.FC = () => {
+    const [players, setPlayers] = useState<ProlificGoalScorer[]>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchPlayers = async () => {
+            try {
+                const data = await getProlificGoalScorer();
+                setPlayers(data.filter((player): player is ProlificGoalScorerer => 
+                    typeof player.playername === 'number'));
+            } catch {
+                setError("Failed to fetch prolific goal scorers.");
+            }
+        };
+
+        console.log(fetchPlayers)
+        fetchPlayers();
+    }, []);
+
+    if (error) {
+        return <div className="text-red-400">{error}</div>;
+    }
+
+
+export default Players;
---
+++ soccerbb/app/players/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Players from '../components/Players';
+
+export default function PlayersPage() {
+  return (
+    <main>
+        <div className="max-w-lg mx-auto p-4 mt-5">
+            <Players  />
+        </div>
+    </main>
+  )
+}
