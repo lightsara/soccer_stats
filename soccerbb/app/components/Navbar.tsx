878f02c7939e700ad2545c3f6a6e1b12c49d43b7
salight <light.sara@icloud.com>
2025-04-07 13:33:45 +0200

    now then

--- soccerbb/app/components/Navbar.tsx
+++ soccerbb/app/components/Navbar.tsx
@@ -1,42 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
-import { ModeToggle } from "./mode-toggle";
-import { ThemeProvider } from "./theme-provider";
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "./separator"
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="flex justify-between bg-blue-500 text-white p-4 theme-secondary-colored theme-children-inherit">
+    <nav className="flex justify-between bg-green-600 text-yellow-500 p-4 theme-secondary-colored theme-children-inherit">
       <ul className="flex items-center space-x-4">
         <li>
           <Link href="/" className="hover:underline">Players</Link>
         </li>
         <Separator orientation="vertical" />
         <li>
-          <Link href="/search" className="hover:underline">Search</Link>
+          <Link href="/teams" className="hover:underline">Teams</Link>
         </li>
         <Separator orientation="vertical" />
         <li>
-          <Link href="/contacts" className="hover:underline">Contacts</Link>
+          <Link href="/leagues" className="hover:underline">Leagues</Link>
         </li>
         <Separator orientation="vertical" />
         <li>
-          <Link href="/posters" className="hover:underline">Most prolific posters</Link>
+          <Link href="/extras" className="hover:underline">Extra special features</Link>
         </li>
       </ul>
-      <div className="theme-break-inherit">
-              <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-                disableTransitionOnChange
-              >
-                <ModeToggle />
-              </ThemeProvider>
-        </div>
       </nav>
   );
 };
-
 
 export default Navbar;
