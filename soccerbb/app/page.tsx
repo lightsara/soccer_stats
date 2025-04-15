67b30ae83f4a38b4f067b7de06cf688e0a82952b
salight <light.sara@icloud.com>
2025-04-15 16:44:26 +0200

    Cleaned up css a bit

--- soccerbb/app/page.tsx
+++ soccerbb/app/page.tsx
@@ -1,4 +1,5 @@
 import * as playerData from '../data/players_cleaned_0414.json';
+import Image from 'next/image';
 
 interface Player {
   player_id: number;
@@ -31,16 +32,15 @@ export default function Home() {
 
     return  ( 
      <div className="container mx-auto p-4">
-          <h1 className="text-2xl font-bold mb-6">Soccer Players</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <h1 className="text-3xl font-bold mb-6">Soccer Players</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
-            {Object.entries(playerData).slice(0, 10).map(([name, info], index) => (
+            {Object.entries(playerData).slice(0, 9).map(([name, info], index) => (
 
             <div key={index} className="border rounded-lg p-4 shadow-sm hover:shadow-md transition-shadow">
               <h2 className="text-lg font-semibold">{typeof info === 'object' && 'name' in info ? info.name : 'N/A'}</h2>
-              {/* Access player info if needed */}
               <p>Club: {typeof info === 'object' && 'current_club_name' in info ? info.current_club_name : 'N/A'} | #Games: {typeof info === 'object' && 'appearances' in info ? info.appearances : 'N/A'}</p>
-              <img src={typeof info === 'object' && 'image_url' in info ? info.image_url : ''} alt={typeof info === 'object' && 'name' in info ? info.name : ''} className="w-full h-auto mt-2 rounded-lg" />
+             <img src={typeof info === 'object' && 'image_url' in info ? info.image_url : ''} alt={typeof info === 'object' && 'name' in info ? info.name : ''} className="w-full h-auto mt-2 rounded-lg" />
             </div>))}
           </div>
       </div>
