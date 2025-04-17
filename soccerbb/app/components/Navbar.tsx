f158c8a6ddd167e28909c2ef26b2791abd45eb0f
salight <light.sara@icloud.com>
2025-04-17 14:55:36 +0200

    rudimentary mods

--- soccerbb/app/components/Navbar.tsx
+++ soccerbb/app/components/Navbar.tsx
@@ -4,22 +4,22 @@ import { Separator } from "./separator"
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="flex bg-green-600 text-yellow-500 p-4 theme-secondary-colored theme-children-inherit">
+    <nav className="flex bg-green-700 text-yellow-500 p-6 theme-secondary-colored theme-children-inherit">
       <ul className="mx-auto flex items-center space-x-20">
         <li>
-          <Link href="/" className="hover:underline">Players</Link>
+          <Link href="/" className="text-2xl font-semibold hover:underline">Players</Link>
         </li>
         <Separator orientation="vertical" />
         <li>
-          <Link href="/teams" className="hover:underline">Clubs</Link>
+          <Link href="/teams" className="text-2xl font-semibold 2hover:underline">Clubs</Link>
         </li>
         <Separator orientation="vertical" />
         <li>
-          <Link href="/leagues" className="hover:underline">Competitions</Link>
+          <Link href="/leagues" className="text-2xl font-semibold hover:underline">Competitions</Link>
         </li>
         <Separator orientation="vertical" />
         <li>
-          <Link href="/extras" className="hover:underline"> Player by Country </Link>
+          <Link href="/extras" className="text-2xl font-semibold hover:underline"> Player by Country </Link>
         </li>
       </ul>
       </nav>
